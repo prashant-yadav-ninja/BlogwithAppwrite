9aec6ef2c6437a0922da7c733813468c6d1b7a1c
Prashant Yadav <prashanty20720@gmail.com>
2024-01-26 18:24:07 +0530

    setup environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(import.meta.env.VITE_DATABASE_URL)
+  
+  // const temp = (import.meta.env.DATABASE_URL) ;
+  // console.log(temp, 'this is coming from assign')
+
 
   return (
     <>
